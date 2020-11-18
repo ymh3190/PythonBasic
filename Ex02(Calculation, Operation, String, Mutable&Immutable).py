6ecdab36790283b36b2dcfa592b375e189d48a8d
ymh3190 <59950687+ymh3190@users.noreply.github.com>
2020-11-19 04:58:45 +0900

    commit

--- Ex02(Calculation, Operation, String, Mutable&Immutable).py
+++ Ex02(Calculation, Operation, String, Mutable&Immutable).py
@@ -1,36 +1,36 @@
-#덧셈
+# 덧셈
 print(5+6)
 
-#곱셈
+# 곱셈
 print(11*-3)
 
-#나누기
+# 나누기
 print(5/3)
 
-#몫
+# 몫
 print(5//3)
 
-#나머지
-print(5%3)
+# 나머지
+print(5 % 3)
 
-#제곱
+# 제곱
 print(3**2)
 print(3**3)
 
-#비교연산자
+# 비교연산자
 """
 >, <, >=, <=, !=, ==
 """
 print(15+9 == 24)
 
-#논리연산자
+# 논리연산자
 """
 and, or
 """
-print(2>3 or 3<5)
-print(2>3 and 3<5)
+print(2 > 3 or 3 < 5)
+print(2 > 3 and 3 < 5)
 
-#문자열
+# 문자열
 print('1234567')
 print("1234567")
 print("abcd\nefg")
@@ -41,19 +41,19 @@ print("""
 789
 """)
 
-#문자열관련함수
+# 문자열관련함수
 print(len("abcdefg"))
 print(len('홍길동'))
 
-name='swplay'
-var1='재미있는 파이썬공부'
-print(var1.replace('공부','study'))
-print(name.upper()) # 대문자로 변환
-var2='ABC'
+name = 'swplay'
+var1 = '재미있는 파이썬공부'
+print(var1.replace('공부', 'study'))
+print(name.upper())  # 대문자로 변환
+var2 = 'ABC'
 print(var2.lower())
 
-a=10
-b='10'
+a = 10
+b = '10'
 print(a)
 print(type(a))
 print(b)
@@ -67,13 +67,5 @@ print(str(a)+b)
 파이썬에는 변할 수 있는 데이터형과 아닌 것이 있다.
 Mutable : list(리스트형), dict(사전형), set(집합형), byte array(바이트 배열형)
 Immutable : numbers(정수,실수,복소수형), string(문자열형), tuple(튜플형),
-            frozenset(불편집합형), bytes(바이트형)
+            frozenset(불변집합형), bytes(바이트형)
 """
-
-
-
-
-
-
-
-
